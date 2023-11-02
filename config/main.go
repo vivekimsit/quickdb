17a0d20b62cac28f74e99c64eea228a26a06b80b
Vivek Poddar <vivekimsit@gmail.com>
2023-11-02 07:14:49 +0530

    implementing INFO and allkeys-random key eviction

--- config/main.go
+++ config/main.go
@@ -3,6 +3,9 @@ package config
 var Host string = "0.0.0.0"
 var Port int = 7379
 
-var KeysLimit int = 5
+var KeysLimit int = 100
 var EvictionStrategy string = "simple-first"
+
+// Will evict EvictionRatio of keys whenever eviction runs
+var EvictionRatio float64 = 0.40
 var AOFFile string = "./quick-master.aof"
--- core/eviction.go
+++ core/eviction.go
@@ -6,12 +6,29 @@ func evict() {
 	switch config.EvictionStrategy {
 	case "simple-first":
 		evictFirst()
+	case "allkeys-random":
+		evictAllkeysRandom()
 	}
 }
 
 func evictFirst() {
 	for k := range store {
-		delete(store, k)
+		Del(k)
 		return
+	}
+}
+
+// Randomly removes keys to make space for the new data added.
+// The number of keys removed will be sufficient to free up least 10% space
+func evictAllkeysRandom() {
+	evictCount := int64(config.EvictionRatio * float64(config.KeysLimit))
+	// Iteration of Golang dictionary can be considered as a random
+	// because it depends on the hash of the inserted key
+	for k := range store {
+		Del(k)
+		evictCount--
+		if evictCount <= 0 {
+			break
+		}
 	}
 }
